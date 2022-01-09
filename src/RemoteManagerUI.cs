aa2ba86323d644ed2f6c64b7355d2e4769bb123a
Kodix86 <cam.peterson@protonmail.com>
2022-01-09 11:41:58 -0600

    Add a few dummy modules.
    We now have a semi functional side bar.
    Need to implement modules before any more GUI functionality can be implemented.

--- src/RemoteManagerUI.cs
+++ src/RemoteManagerUI.cs
@@ -3,13 +3,46 @@ namespace RemoteManager;
 using Gtk;
 
 public class RemoteManagerUI : Window {
+    //Instances of elements in gui
+    #region Properties
+    
+    [Builder.Object]
+    private Gtk.ListBox ModuleSelector = null!;
+    
+    #endregion
+
     private Builder _builder;
+    private Module[] _availableModules;
+
     private RemoteManagerUI(Builder builder, IntPtr handle) : base(handle) {
         _builder = builder;
 
         builder.Autoconnect(this);
+
+        _availableModules = RetrieveAvailableModules();
+        EmployModules(_availableModules);
         
-        DeleteEvent += delegate { Application.Quit(); };
+        DeleteEvent += delegate { Application.Quit(); }; //Simple anon method to close when exit button pressed.
+    }
+
+    private void EmployModules(IEnumerable<Module> modules) {
+        foreach (var module in modules) {
+            var newElem = new ListBoxRow();
+            var label = new Label("Module");
+            
+            newElem.SetProperty("visible", new GLib.Value(true));
+            label.SetProperty("visible", new GLib.Value(true));
+            newElem.Add(label);
+        
+            var mSel = (ListBox) _builder.GetObject("ModuleSelector");
+        
+            mSel.Add(newElem);
+        }
+    }
+
+    private static Module[] RetrieveAvailableModules() {
+        return new Module[3];
+        // return Array.Empty<Module>();
     }
 
     public static RemoteManagerUI Create() {
